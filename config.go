06699dc80cf425aa18c43100331f268b68c7c84c
Li-aung 'Lewis' Yip <liaung@gmail.com>
2016-05-09 04:33:01 +0800

    Up version to flush cache.

--- config.go
+++ config.go
@@ -5,4 +5,4 @@ var databaseFilename = "glyphbot.boltdb"
 // Used inside the cache DB.
 // Incrementing this version number invalidates the cache, causing subsequent
 // images to be created from scratch.
-var programVersion = "0.0.2"
+var programVersion = "0.0.3"
