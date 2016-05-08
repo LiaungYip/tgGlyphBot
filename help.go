fb9377200cec23aeebe009d8aae68b0039cf4b3e
Li-aung 'Lewis' Yip <liaung@gmail.com>
2016-05-09 04:32:46 +0800

    Tweaking help

--- help.go
+++ help.go
@@ -5,13 +5,13 @@ var helpString = `<b>Ingress Glyph Bot</b>
 
 This bot makes stickers of Ingress glyphs and glyph sequences.
 
-Try sending it messages like this:
+Open a conversation with the bot, and try sending it messages like this:
 
 ► One glyph: <code>Portal</code>
 ► Multiple glyphs: <code>Clear All, Open All, Discover, Truth</code>
 ► Un-used glyphs: <code>N'Zeer, Victory</code>
 
-Or you can add it to your chat, and use it like this:
+Or you can add the bot as a member of your chat, and use it like this:
 
 ► <code>/glyphs Breathe, Inside, XM, Lose, Self</code>
 
@@ -20,10 +20,10 @@ Or you can add it to your chat, and use it like this:
 This is <b>beta software</b>. If the bot isn't responding, it's probably crashed. Message @liaungyip and complain!
 
 ► The glyph names must be spelled exactly like in the scanner / Glyphtionary.
-    ✓ <code>Portal</code>
-    ✗ <code>Portals</code> (Extra <code>s</code>)
+    ✓ <code>Portal, Human</code>
+    ✗ <code>Portals, Humans</code> (Extra <code>s</code>)
 
-► The glyph names must be separated using commas - not spaces, semicolons, dashes, or anything else.
+► The glyph names must be separated using commas. If you forget the commas, it won't work.
     ✓ <code>Help, Enlightened, Capture, All, Portal</code>
     ✗ <code>Help Enlightened Capture All Portal</code>
 
